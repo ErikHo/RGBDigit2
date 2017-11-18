ba71f132b0f5b4a9d793ba6e22647e9c34c6d6d7
ErikHo <f.g.a.homburg@gmail.com>
2017-11-18 22:16:00 +0100

    Add files via upload

---
+++ src/RGBDigit2.cpp
@@ -0,0 +1,169 @@
+/*  RGBDigit2 class cpp file
+    Copyright (C) 2017 Erik Homburg
+    Copyright (C) 2015 Ralph Cr�tzen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "RGBDigit2.h"  
+
+RGBDigit::RGBDigit(int nDigits, int pin)
+  : Adafruit_NeoPixel(8 * nDigits, pin, NEO_GRB + NEO_KHZ800){
+}
+
+void RGBDigit::begin(){
+  Adafruit_NeoPixel::begin();
+}
+
+void RGBDigit::clearAll(){
+  clear();
+  show();
+}
+
+void RGBDigit::setDigit(char x, int digit, uint32_t c){
+  setDigit( x, digit, byte(c >> 16), byte(c >>8 ), byte(c));
+}
+
+void RGBDigit::setDigit(char x, int digit, byte red, byte green, byte blue){
+  x = tolower(x);                     // convert to lower case
+  byte pattern;
+  if((x >= 'a') && (x <='z')){        // get bit pattern for a letter
+    pattern = alphaNum[x-int('a')+10];
+  }
+  else if((x >= '0') && (x <='9')){   // get bit pattern for a digit
+    pattern = alphaNum[x-int('0')];
+  }
+  else if((x >= 0) && (x <=0x0F)){    // direct selection of pattern for 
+    pattern = alphaNum[x];            // a (hex) number
+  }
+  else{                               // special characters        
+    switch(x){
+    case '.': 
+      pattern= B10000000;  //space, '.'
+    break;
+    case ' ':
+      pattern = B00000000;  //space,' '
+    break;
+    case '-':
+      pattern = B01000000;  //minus,'-'
+    break;
+    case '_':
+      pattern = B00001000;  //underscore,'_' 
+    break;
+    case '{':
+    case '[':
+    case '(':
+      pattern = B00111001;  //leftbracket, '('
+    break;
+    case '}':
+    case ']':
+    case ')':
+      pattern = B00001111;   //rightbracket,')'
+    break;
+    case '*':
+      pattern = B01100011;   //asterisk,'*', used as degree symbol
+    break;
+    default:
+      pattern = B01001001;   //undefined character = 3 horizontal bars
+    break;
+    }
+  }
+  byte bitMask = 1;
+  for (int i = 0; i < 8; i++){
+    if (pattern & bitMask){
+      setPixelColor((digit <<3)+i, red, green, blue);
+    }
+    else{
+      setPixelColor((digit <<3)+i, 0, 0, 0);
+    }
+    bitMask <<= 1;
+  }
+  show();
+}
+
+
+void RGBDigit::clearDigit(int digit){
+  for (int i = 0; i < 8; i++) {
+    setPixelColor((digit << 3) +i, 0, 0, 0);
+  }
+  show();
+}
+
+void RGBDigit::showDot(int digit, byte red, byte green, byte blue){
+  setPixelColor((digit << 3) + 7, red, green, blue);
+  show();
+}
+
+void RGBDigit::showDot(int digit, uint32_t c){
+  setPixelColor((digit << 3) + 7, c);
+  show();
+}
+
+void RGBDigit::clearDot(int digit){
+  setPixelColor((digit << 3) + 7, 0, 0, 0);
+  show();
+}
+
+void RGBDigit::segmentOn(int digit, byte segment, byte red, byte green, byte blue){
+  setPixelColor((digit << 3) + segment, red, green, blue);
+  show();
+}
+
+void RGBDigit::segmentOn(int digit, byte segment, uint32_t c){
+  setPixelColor((digit << 3) + segment, c);
+  show();
+}
+
+void RGBDigit::segmentOff(int digit, byte segment){
+  setPixelColor((digit << 3) + segment, 0, 0, 0);
+  show();
+}
+
+bool RGBDigit::isSegmentOn(int digit, byte segment){
+  return (getPixelColor((digit << 3) + segment) != 0);
+}
+
+void RGBDigit::setColor(byte red, byte green, byte blue){
+  int nDigits = numPixels() << 3;
+  for(int digit=0; digit < nDigits; digit++){
+    setColor(digit, red, green, blue);
+  }
+}
+
+void RGBDigit::setColor(uint32_t c){
+  int nDigits = numPixels() << 3;
+  for(int digit=0; digit < nDigits; digit++){
+    setColor(digit, c);
+  }
+}
+
+void RGBDigit::setColor(int digit, byte red, byte green, byte blue){
+  for (int i = 0; i < 7; i++){
+    if (getPixelColor(((digit  << 3) + i)) != 0) {
+        setPixelColor(((digit  << 3) + i), red, green, blue);
+    }
+  }
+  show();
+}
+
+void RGBDigit::setColor(int digit, uint32_t c){
+  for (int i = 0; i < 7; i++) {
+    if (getPixelColor(((digit  << 3) + i)) != 0) {
+        setPixelColor(((digit  << 3) + i), c);
+    }
+  }
+  show();
+}
+
---
+++ src/RGBDigit2.h
@@ -0,0 +1,166 @@
+/*  RGBDigitLean class header file
+    Copyright (C) 2017 Erik Homburg
+    Copyright (C) 2015 Ralph Crützen
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef RGBDigit2_h
+#define RGBDigit2_h
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h> //https://github.com/adafruit/Adafruit_NeoPixel
+
+
+/*
+The RGBdigit class is an interface to RGBDigits, a 7 segment display based on NeoPixel
+leds (Adafruiit NeoPixel). Multiple RGBDigits can form a display line. Each RGBdigit 
+contains a string of 8 NeoPixels arranged in the classical 7 segment configuration. 
+The segments are adressed with a segment parameter 0..7 indicating the segments A..G
+and the decimal point P:
+
+         AAA         AAA         AAA         AAA       
+       F     B     F     B     F     B     F     B    
+       F     B     F     B     F     B     F     B  
+         GGG         GGG         GGG         GGG
+       E     C     E     C     E     C     E     C                      
+       E     C     E     C     E     C     E     C
+         DDD   P     DDD   P     DDD   P     DDD   P
+         
+       digit 0     digit 1     digit 2     digit 3  
+
+Multiple RGBDigits are adressed with a parameter digit that is zero-based: the first RGBDigit
+is 0, second is 1, etc.
+         
+This software is a fine tuning of the work of Ralph Crützen 2015 (www.rgbdigit.com). The header
+is adapted only slightly. Most software should run on this new class without any modification.
+The main reason for thes changes is the reduction of the RAM memory footprint of the class.
+
+Mayor changes with respect to the original version:
+
+- The encoding matrices for alphanumeric characters are changed into a more packed format: 8 bits
+  are moved into one byte giving 8 fould smaller memory footprint. Both matrices are also combined
+  into a single array. The encoding of special characters is moved into the class code, yielding
+  some more free bytes of RAM.
+  
+- The array containing the color values for the pixel string has been removed. The underlying 
+  Adafruit_NeoPixel class already has such an array. The Adafruit implementation is somewhat lossy:
+  at lower brightness levels the colorvalues in the array get changed due to truncation effects.
+  For the functions in this class this effect is not important. Otherwise one has to refrain from 
+  changing the display brightness with the setBrightness function.
+
+- Both setDigit methods with char and int parameters are merged into a single method.
+
+- Pixel colors can be specified with red,green,blue triplets and with the packed uint32_t type.
+  The latter is handy if you want to use color names.
+  
+- The setBrightness and getBrightness are removed because they are already present in the underlying
+  NeoPixel class.
+
+*/
+
+
+class RGBDigit : public Adafruit_NeoPixel {
+  public:
+    // The constructor of the RGBDigit class. 
+    // nDigits is the number of digits. pin is the pin number, 
+    // which defaults to pin 12 if this parameter is omitted.
+    RGBDigit(int nDigits, int pin = 12);
+    
+    // Initialises RGBDigit:
+    void begin();
+    
+    // Clear all digits:
+    void clearAll();
+    
+    // Show a character on digit in color rgb(red,green,blue)
+    // If the ascii value of char is between 0 and 15 a hexadecimal 
+    // symbol 0..F is shown. This of course include 0..9 in decimal.
+    // otherwise how character on digit. Valid characters are letters
+    // (case insensitive) from a to z, the dot (.), dash (-), 
+    // underscore (_), brackets ( ( ) { } [ ] ) and space . A special 
+    // character (3 bars) is used for ascii values not supported.
+    // Use an asterisk (*) for the degree sign:
+    void setDigit(char x, int digit,  byte red, byte green, byte blue );
+    // Show dot on digit in color rgb(red,green,blue):
+    void showDot(int digit, byte red, byte green, byte blue);
+    // Show segment on digit in color rgb(red,green,blue):
+    void segmentOn(int digit, byte segment, byte red, byte green, byte blue);
+    //Set the color of all digits to color rgb(red,green,blue), 
+    // do not change decimal points:
+    void setColor(byte red, byte green, byte blue);
+    // Set the color of digit to color rgb(red,green,blue),
+    // do not change the decimal point:
+    void setColor(int digit, byte red, byte green, byte blue);
+    
+    // uint32_t versions:
+    void setDigit(char x, int digit,  uint32_t c);
+    void showDot(int digit, uint32_t c);
+    void segmentOn(int digit, byte segment, uint32_t c);
+    void setColor(uint32_t c);
+    void setColor(int digit, uint32_t c);
+    
+    // Clear digit:
+    void clearDigit(int digit);
+    // Clear dot on digit:
+    void clearDot(int digit);
+    // Clear segment on digit:
+    void segmentOff(int digit, byte segment);
+    //Returns true if segment on digit is on. Otherwise, returns false:
+    bool isSegmentOn(int digit, byte segment); 
+  
+  private:
+    const byte alphaNum[37] = {
+    // .GFEDCBA
+      B00111111,     //0      This character bitmap is a direct translation
+      B00000110,     //1      from Crützen 2015
+      B01011011,     //2
+      B01001111,     //3      The rightmost bit controls the A segment and the 
+      B01100110,     //4      leftmost bit controls the decimal point
+      B01101101,     //5
+      B01111101,     //6
+      B00000111,     //7
+      B01111111,     //8
+      B01101111,     //9
+      B01110111,     //a
+      B01111100,     //b  
+      B00111001,     //c
+      B01011110,     //d
+      B01111001,     //e
+      B01110001,     //f
+      B01101111,     //g
+      B01110100,     //h
+      B00000110,     //i
+      B00001110,     //j 
+      B01110110,     //k
+      B00111000,     //l
+      B11010100,     //m  
+      B01010100,     //n  
+      B01011100,     //o
+      B01110011,     //p
+      B01100111,     //q
+      B01010000,     //r
+      B01101101,     //s
+      B01111000,     //t
+      B00111110,     //u
+      B00011100,     //v
+      B10011100,     //w
+      B11110110,     //x
+      B01101110,     //y
+      B01011011      //z
+    };    
+};
+
+#endif
